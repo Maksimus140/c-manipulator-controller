01ea2ea70a07e4d43ab5762453e174e2c8d79766
Jan Nowak <maksimusss1234@gmail.com>
2026-09-17 23:43:50 +0200

    zmiana wyszukiwania komend

--- manipulator.c
+++ manipulator.c
@@ -1,12 +1,31 @@
-#include  <stdio.h>
+#include  <stdio.h>// zrobic to ze pozycja targetu gdy jest podniesiony rowna sie pozycji manipulatora
 #include <stdbool.h>// jako werjsa 1.1 probowac pozbyc sie tylu ifow
+int error_number = 0;
+int part_in_magazine = 0;
+int what_to_do_menu = 0;
 typedef struct //dodac tutaj tryb auto i manual jako druga wersja
 {
-    bool part_detected; //dodac error jako struct i jako tabele zeby miec nazwe erroru i numer jego zeby wyszukiwac errory jako nastepny updejcik
+    bool part_detected; 
     bool part_gripped;
     bool part_in_magazine;
-    //bool manipulator_home;
 } Sensors;
+typedef struct
+{
+    int error_numbers;
+    char const *message;
+}Error_info; 
+
+const Error_info error_infoo  [] =
+{
+    {1, "nie ma takiego wyboru\r\n"},
+    {2, "nie ma takiego wyboru\r\n"},
+    {3, "sensor nie wykrywa partu\r\n"},
+    {4, "manipulator nie znajduje sie przy targecie\r\n"},
+    {5, "nie ma zadnego targetu na mapie\r\n"},
+    {6, "figurka nie trafila do magazynu\r\n"},
+    {7, "nie podniosles manipulatorem figurki\r\n"},
+};
+
 typedef enum
 {
     STATE_MENU,
@@ -14,9 +33,9 @@ typedef enum
     STATE_MAGAZINE,
     STATE_PICK,
     STATE_SETTINGS,
-    STATE_ERROR,
     STATE_DROP,
     STATE_STATUS,
+    STATE_ERROR,
 }Menu;
 typedef struct
 {
@@ -36,91 +55,47 @@ typedef struct
     Menu menu;
     Target target;
     Sensors sensors;
+    const Error_info *error_info;
 
 }Machine;
-
-void working_machine(Machine *machine)
+void (*struct_tabel[8])[Machine *]
 {
-    int error_number = 0;
-    int part_in_magazine = 0;
-    int what_to_do_menu = 0;
-    machine->menu = STATE_MENU;
-    while (true)
-    {
-    if(machine->menu == STATE_MENU)
-    {
-        printf("co chcesz zrobic\r\n"
-        "1.wejsc do ustawien\r\n"
-        "2.podniesc figurke\r\n"
-        "3.ruszyc sie w powierzchni x\r\n"
-        "4.sprawdzic magazyn \r\n"
-        "5.odlozyc figurke do magazynu\r\n"
-        "6.sprawdzic status maszyny\r\n");
-        scanf("%d", &what_to_do_menu);
-        if(what_to_do_menu == 1)
-        {
-            machine->menu = STATE_SETTINGS;
-        }
-        else if(what_to_do_menu == 2)
-        {
-            machine->menu = STATE_PICK;
-        }
-        else if(what_to_do_menu == 3)
-        {
-            machine->menu = STATE_MOVE_X;
-        }
-        else if(what_to_do_menu == 4)
-        {
-            machine->menu = STATE_MAGAZINE;
-        }
-        else if(what_to_do_menu == 5)
-        {
-            machine->menu = STATE_DROP;
-        }
-        else if(what_to_do_menu == 6)
-        {
-            machine->menu = STATE_STATUS;
-        }
-        else
-        {
-            error_number = 1;
-            machine->menu = STATE_ERROR;
-        }
-    }
-    if(machine->menu == STATE_SETTINGS)
-    {
+
+};
+
+
+void move_x(Machine *machine)
+{
+        int move_x = 0;
+        printf("o ile chcesz sie ruszyc w pozycji x\r\n");
+        scanf("%d", &move_x);
+        machine->manipulator.position_x += move_x;
+        machine->menu = STATE_MENU;
+        struct_tabel[machine->menu](machine);
+}
+void settings(Machine *machine)
+{
         int what_to_do_settings = 0;
-        printf("co chcesz zrobic\n"
-        "1.ustawic pozycje x targetu\n");
+        printf("co chcesz zrobic\r\n"
+        "1.ustawic pozycje x targetu\r\n");
         scanf("%d", &what_to_do_settings);
         if(what_to_do_settings == 1)
         {
-            printf("podaj pozycje x");
+            printf("podaj pozycje x\r\n");
                 scanf("%d", &machine->target.position_x);
                 machine->target.target_on_map = true;
                 machine->menu = STATE_MENU;
+                struct_tabel[machine->menu](machine);
         }
         else
         {
                 error_number = 2;
                 machine->menu = STATE_ERROR;
+                struct_tabel[machine->menu](machine);
         }
-    }
-    if(machine->menu == STATE_MOVE_X)
-    {
-        int move_x = 0;
-        printf("o ile chcesz sie ruszyc w pozycji x");
-        scanf("%d", &move_x);
-        machine->manipulator.position_x += move_x;
-        machine->menu = STATE_MENU;
-    }
-    if(machine->menu == STATE_MAGAZINE)
-    {
-        printf("%d", part_in_magazine);
-        machine->menu = STATE_MENU;
-    }
-    if(machine->menu == STATE_PICK)
-    {
+}
+void pick(Machine *machine)
+{
         if(machine->target.target_on_map)
         {
             machine->sensors.part_detected = true;
@@ -128,37 +103,47 @@ void working_machine(Machine *machine)
                     {
                         if(machine->sensors.part_detected)
                         {
-                            printf("manipulator znajduje sie przy obiekcie");
+                            printf("manipulator znajduje sie przy obiekcie\r\n");
                             machine->manipulator.gripper = true;
                             machine->sensors.part_gripped = true;
                             machine->target.position_x = 0;
                             machine->sensors.part_detected = false;
                             machine->target.target_on_map = false;
                             machine->menu = STATE_MENU;
+                            struct_tabel[machine->menu](machine);
                         }
                         else
                         {
                             error_number = 3;
                             machine->menu = STATE_ERROR;
+                            struct_tabel[machine->menu](machine);
                         }
                     }
                     else
                     {
                         error_number = 4;
                         machine->menu = STATE_ERROR;
+                        struct_tabel[machine->menu](machine);
                     }
         }
         else if(!machine->target.target_on_map)
         {
             error_number = 5;
             machine->menu = STATE_ERROR;
+            struct_tabel[machine->menu](machine);
         }
-    }
-    if(machine->menu == STATE_DROP)
-    {
+}
+void magazine(Machine *machine)
+{
+        printf("%d", part_in_magazine);
+        machine->menu = STATE_MENU;
+        struct_tabel[machine->menu](machine);
+}
+void drop(Machine *machine)
+{
         if(machine->manipulator.gripper && machine->sensors.part_gripped)
         {
-            printf("wrzucasz figurke do magazynu");
+            printf("wrzucasz figurke do magazynu\r\n");
             machine->manipulator.gripper = false;
             machine->sensors.part_gripped = false;
             machine->sensors.part_in_magazine = true;
@@ -167,11 +152,13 @@ void working_machine(Machine *machine)
                 part_in_magazine += 1;
                 machine->sensors.part_in_magazine = false;
                 machine->menu = STATE_MENU;
+                struct_tabel[machine->menu](machine);
             }
             else
             {
                 error_number = 6;
                 machine->menu = STATE_ERROR;
+                struct_tabel[machine->menu](machine);
             }
 
 
@@ -180,60 +167,75 @@ void working_machine(Machine *machine)
         {
             error_number = 7;
             machine->menu = STATE_ERROR;
+            struct_tabel[machine->menu](machine);
         }
-    }
-    if(machine->menu == STATE_STATUS)
-    {
+}
+void status(Machine *machine)
+{
         printf("na jakim x znajduje sie manipulator - %d\r\n na jakim x znajduje sie target - %d\r\n", machine->manipulator.position_x, machine->target.position_x);
         machine->menu = STATE_MENU;
-    }
-    if(machine->menu == STATE_ERROR)
-    {
-        switch(error_number)
-        {
-            case 1:
-                printf("nie ma takiego wyboru");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-            case 2:
-                printf("nie ma takiego wyboru");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-            case 3:
-                printf("sensor nie wykrywa partu");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-            case 4:
-                printf("manipulator nie znajduje sie przy targecie");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-            case 5:
-                printf("nie ma zadnego targetu na mapie");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-            case 6:
-                printf("figurka nie trafila do magazynu");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-            case 7:
-                printf("nie podniosles manipulatorem figurki");
-                error_number = 0;
-                machine->menu = STATE_MENU;
-                break;
-
-        }
-    }
+        struct_tabel[machine->menu](machine);
 }
+void menu(Machine *machine);
+
+void error(Machine *machine)
+{
+        for(int i = 0; i < 7; i++)
+        {
+            if(machine->error_info[i].error_numbers == error_number)
+            {
+                printf("%s", machine->error_info[i].message);
+                error_number = 0;
+                machine->menu = STATE_MENU;     
+                struct_tabel[machine->menu](machine);     
+            }
+        }
+}
+struct_tabel[0] = menu;
+struct_tabel[1] = move_x;
+struct_tabel[2] = magazine;
+struct_tabel[3] = pick;
+struct_tabel[4] = settings;
+struct_tabel[5] = drop;
+struct_tabel[6] = status;
+struct_tabel[7] = error;
+void (*struct_tabel[])(Machine *) =
+{
+    menu,
+    move_x,
+    magazine,
+    pick,
+    settings,
+    drop,
+    status,
+    error
+};
+void menu(Machine *machine)
+{
+        printf("co chcesz zrobic\r\n"
+        "1.rusz manipulator\r\n"
+        "3.sprawdz magazyn\r\n"
+        "3.podniesc przedmiot\r\n"
+        "4.przejdz do ustawien\r\n"
+        "5.odloz figurke do magazynu\r\n"
+        "6.sprawdzic status maszyny\r\n");
+        scanf("%d", &what_to_do_menu);
+        struct_tabel[what_to_do_menu](machine);
+}
+
+void working_machine(Machine *machine)
+{
+    while (true)
+    {
+        struct_tabel[0](machine);
+
+
+    }
 }
 int main(void)
 {
     Machine machine = {0};
+    machine.error_info = error_infoo;
     working_machine(&machine);
     return 0;
 }
